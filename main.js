755b2e0d3c690533666a6acf2233b5cb2134c22a
LELEE0407 <lelee040700@gmail.com>
2024-06-30 10:05:28 +0900

    Git practice

---
+++ main.js
@@ -0,0 +1,8 @@
+let persons = [
+    { id: 1, name: "Le Van Nghia", age: 23 },
+    { id: 2, name: "Le Tai", age: 24 },
+    { id: 3, name: "Le Uy", age: 25 },
+    { id: 4, name: "Le lee", age: 26 }
+]
+
+document.getElementById("")
